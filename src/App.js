11c8b6bd838683805c68584c19c17b2ee52e67a8
MPasti <matheusspasti@gmail.com>
2023-07-14 13:19:38 -0300

    criando tela de login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 
 function App() {
+  const handleClickLogin = (values) => console.log(values);
+  const handleClickRegister = (values) => console.log(values);
+
+  const validationLogin = yup.object().shape({
+    email: yup
+      .string()
+      .email("Não é um email válido")
+      .required("Este campo é obrigatório"),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter pelo menos 8 caracteres")
+      .required("Este campo é obrigatório"),
+  });
+
+  const validationCadastro = yup.object().shape({
+    email: yup
+      .string()
+      .email("Não é um email válido")
+      .required("Este campo é obrigatório"),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter pelo menos 8 caracteres")
+      .required("Este campo é obrigatório"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Senhas não correspondem"),
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Login</h1>
+      <Formik
+        initialValues={{}}
+        onSubmit={handleClickLogin}
+        validationSchema={validationLogin}
+      >
+        <Form className="login-form">
+          <div className="login-form-group">
+            <Field
+              name="email"
+              className="form-field"
+              placeholder="Email"
+            ></Field>
+            <ErrorMessage
+              component="span"
+              name="email"
+              className="form-error"
+            />
+          </div>
+          <div className="login-form-group">
+            <Field
+              name="password"
+              className="form-field"
+              placeholder="Senha"
+              type="password"
+            ></Field>
+            <ErrorMessage
+              component="span"
+              name="password"
+              className="form-error"
+            />
+          </div>
+          <button className="button" type="submit">
+            Login
+          </button>
+        </Form>
+      </Formik>
+
+      <h1>Cadastro</h1>
+      <Formik
+        initialValues={{}}
+        onSubmit={handleClickRegister}
+        validationSchema={validationCadastro}
+      >
+        <Form className="login-form">
+          <div className="login-form-group">
+            <Field
+              name="email"
+              className="form-field"
+              placeholder="Email"
+            ></Field>
+            <ErrorMessage
+              component="span"
+              name="email"
+              className="form-error"
+            />
+          </div>
+          <div className="login-form-group">
+            <Field
+              name="password"
+              className="form-field"
+              placeholder="Senha"
+            ></Field>
+            <ErrorMessage
+              component="span"
+              name="password"
+              className="form-error"
+            />
+          </div>
+          <div className="login-form-group">
+            <Field
+              name="confirmPassword"
+              className="form-field"
+              placeholder="Confirme sua senha"
+            ></Field>
+            <ErrorMessage
+              component="span"
+              name="confirmPassword"
+              className="form-error"
+            />
+          </div>
+          <button className="button" type="submit">
+            Login
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 }
